aca50a248ddacb824ca51ca7f6a94fe1990b03ec
shamim496 <mdshamimhasan496@gmail.com>
2025-02-25 10:28:11 +0600

    update release

--- zolo-accordion.php
+++ zolo-accordion.php
@@ -6,8 +6,8 @@
  * Requires at least: 6.1
  * Requires PHP:      7.4
  * Version:           1.0.0
- * Author:            Shamim Hasan
- * Author URI:        https://shamim496.com
+ * Author:            BdThemes
+ * Author URI:        https://bdthemes.com
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       zolo-accordion
