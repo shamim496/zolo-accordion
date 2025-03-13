ea5b0f036a7252344dd4b0e5e115b91123e014eb
shamim496 <mdshamimhasan496@gmail.com>
2025-03-13 15:45:08 +0600

    frontend style issue fix

--- includes/StyleGenerator.php
+++ includes/StyleGenerator.php
@@ -64,7 +64,7 @@ if (!class_exists('ZoloLibrary\Includes\StyleGenerator')) {
         }
 
         public function output_dynamic_styles() {
-            wp_enqueue_style('zolo-block-inline-styles', plugin_dir_url(__FILE__) . 'style.css', array(), ZOLO_ACCORDION_VERSION);
+            wp_enqueue_style('zolo-block-inline-styles', trailingslashit( ZOLO_ACCORDION_URL ).'dist/zoloLibrary.css', [], ZOLO_ACCORDION_VERSION);
         
             if (!empty($this->dynamic_styles)) {
                 wp_add_inline_style('zolo-block-inline-styles', $this->dynamic_styles);
